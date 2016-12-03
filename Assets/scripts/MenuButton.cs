5b3ec29311992da284c72665b9f947b1c140e670
Anthony Robinson <aerobins12994@gmail.com>
2016-12-02 20:32:25 -0500

    menu update(how to play)

--- Assets/scripts/MenuButton.cs
+++ Assets/scripts/MenuButton.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MenuButton : MonoBehaviour {
 
     public Button playB;
+    public Button how2playB;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,10 @@ public class MenuButton : MonoBehaviour {
            //Application.LoadLevel("demoLevel"); //This is obsolete now, they now provide an async way to call this for better performance
            SceneManager.LoadSceneAsync("demoLevel");
        });
+
+        how2playB.onClick.AddListener(() =>
+        {
+            SceneManager.LoadSceneAsync("how2play");
+        });
 	}
 }
